ede185f5ccf570543643f3638150697fa28d38e6
JUNKI555 <junki555@gmail.com>
2021-02-12 13:58:22 +0900

    :wrench: rails g controller Home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  root to: "home#index"
+  
+  ## 開発環境用letter_opener
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
