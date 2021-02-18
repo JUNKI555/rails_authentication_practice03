5aa5d5232f8f82de5dcf68198688be82373412a2
JUNKI555 <junki555@gmail.com>
2021-02-18 14:41:22 +0900

    :sparkles: setup confirmations with devise split models

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,30 +1,44 @@
 # frozen_string_literal: true
 
 class Users::ConfirmationsController < Devise::ConfirmationsController
-  # GET /resource/confirmation/new
-  # def new
-  #   super
-  # end
+  # GET /users/confirmation/new
+  def new
+    super
+  end
 
-  # POST /resource/confirmation
-  # def create
-  #   super
-  # end
+  # POST /users/confirmation
+  def create()
+    registered = UserDatabaseAuthentication.where(email: params[:user_registration][:email]).exists?
 
-  # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
+    if registered
+      flash[:error] = '既に登録されているメールアドレスです。'
+      return render :new
+    end
 
-  # protected
+    user_registration = UserRegistration.find_or_initialize_by(unconfirmed_email: params[:user_registration][:email])
+    if user_registration.save
+      super
+    else
+      flash[:error] = '処理に失敗しました。もう一度お試しください。'
+      return render :new
+    end
+  end
 
-  # The path used after resending confirmation instructions.
-  # def after_resending_confirmation_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  # GET /users/confirmation?confirmation_token=abcdef
+  def show
+    # super 側で confirmation_token　の検証をしてリダイレクト
+    super
+  end
 
-  # The path used after confirmation.
-  # def after_confirmation_path_for(resource_name, resource)
-  #   super(resource_name, resource)
-  # end
+  protected
+
+   # 確認メールを送信した後に使用する path
+  def after_resending_confirmation_instructions_path_for(resource_name)
+    root_path
+  end
+
+  # 確認後に使用する path
+  def after_confirmation_path_for(resource_name, resource)
+    new_user_database_authentication_registration_path
+  end
 end
